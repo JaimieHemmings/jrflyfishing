1c9356eda3eae1b1385098cdbc6ace23648499c9
Jaimie <jaimie.j.hemmings@gmail.com>
2024-11-17 11:41:32 +0000

    add: categories to thoughts model

--- thoughts/admin.py
+++ thoughts/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
-from .models import Thought
+from .models import Thought, Category
+
 
 class ThoughtAdmin(admin.ModelAdmin):
   exclude = ('slug', 'id', 'created_at', 'updated_at')
 
+
+class CategoryAdmin(admin.ModelAdmin):
+  exclude = ('slug', 'id', 'created_at', 'updated_at')
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Thought, ThoughtAdmin)
--- thoughts/models.py
+++ thoughts/models.py
@@ -5,6 +5,40 @@ import random
 import string
 from ckeditor.fields import RichTextField
 
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255, help_text='Name of the category')
+    slug = models.SlugField(max_length=255, help_text='Slug of the category for SEO purposes')
+    description = models.TextField(help_text='Description of the category for SEO purposes')
+    created_at = models.DateTimeField(auto_now_add=True, help_text='Date and time of creation')
+    updated_at = models.DateTimeField(auto_now=True, help_text='Date and time of last update')
+
+    def save(self, *args, **kwargs):
+        self.slug = self.generate_slug()
+        return super().save(*args, **kwargs)
+  
+    def generate_slug(self, save_to_obj=False, add_random_suffix=True):
+        generated_slug = slugify(self.name)
+      
+        random_suffix = ""
+        if add_random_suffix:
+            random_suffix = ''.join([
+                random.choice(string.ascii_letters + string.digits)
+                for i in range(5)
+            ])
+            generated_slug += '-%s' % random_suffix
+
+        if save_to_obj:
+            self.slug = generated_slug
+            self.save(update_fields=['slug'])
+        
+        return generated_slug
+  
+    def __str__(self):
+        return self.name
+
+
 class Thought(models.Model):
     
     def get_path(self, filename):
@@ -18,6 +52,8 @@ class Thought(models.Model):
 
     banner_image = models.ImageField(upload_to=get_path, help_text='Image of the thought')
     thumbnail_image = models.ImageField(upload_to=get_path, help_text='Thumbnail image of the thought')
+
+    categories = models.ManyToManyField(Category, help_text='Categories of the thought')
 
 
     title = models.CharField(max_length=255, help_text='Title of the thought')
